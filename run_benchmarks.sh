f35ffab38079311e68748fc0a3aa9b6d76cb9c7a
Oliver Calder <oacalder@gmail.com>
2020-08-27 19:10:34 -0500

    Added benchmark output directory to keep project directory clean.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+ID="$(date +%s)"
+OUTDIR="Benchmark-$ID"
+
 # Linux (non-kvm)
 run_linux() {
     echo "Benchmarking linux with timestep of $1 trial 1"
-    sh benchmark_linux.sh -o linux-results-r"$1".txt -p linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_linux.sh -o linux-results-r"$1".txt -p "$OUTDIR"/linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking linux with timestep of $1 trial 2"
-    sh benchmark_linux.sh -o linux-results-r"$1".txt -p linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_linux.sh -o linux-results-r"$1".txt -p "$OUTDIR"/linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking linux with timestep of $1 trial 3"
-    sh benchmark_linux.sh -o linux-results-r"$1".txt -p linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_linux.sh -o linux-results-r"$1".txt -p "$OUTDIR"/linux-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 2> /dev/null
 }
 run_linux 4
 run_linux 2
@@ -16,11 +19,11 @@ run_linux 1
 # Linux (kvm)
 run_linux_kvm() {
     echo "Benchmarking linux-kvm with timestep of $1 trial 1"
-    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
+    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p "$OUTDIR"/linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
     echo "Benchmarking linux-kvm with timestep of $1 trial 2"
-    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
+    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p "$OUTDIR"/linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
     echo "Benchmarking linux-kvm with timestep of $1 trial 3"
-    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
+    sh benchmark_linux.sh -o linux-kvm-results-r"$1".txt -p "$OUTDIR"/linux-kvm-output-r"$1" -i qemu_image_timescript.img -n -r "$1" -w 120 -t 60 -k 2> /dev/null
 }
 run_linux_kvm 4
 run_linux_kvm 2
@@ -33,11 +36,11 @@ run_linux_kvm 0.0625
 # Rust
 run_rust() {
     echo "Benchmarking Rust with timestep of $1 trial 1"
-    sh benchmark_rust.sh -o rust-results-r"$1".txt -p rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_rust.sh -o rust-results-r"$1".txt -p "$OUTDIR"/rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking Rust with timestep of $1 trial 2"
-    sh benchmark_rust.sh -o rust-results-r"$1".txt -p rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_rust.sh -o rust-results-r"$1".txt -p "$OUTDIR"/rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking Rust with timestep of $1 trial 3"
-    sh benchmark_rust.sh -o rust-results-r"$1".txt -p rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_rust.sh -o rust-results-r"$1".txt -p "$OUTDIR"/rust-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
 }
 run_rust 4
 run_rust 2
@@ -55,11 +58,11 @@ run_rust 0.001953125
 # Docker
 run_docker() {
     echo "Benchmarking Docker with timestep of $1 trial 1"
-    sh benchmark_docker.sh -o docker-results-r"$1".txt -p docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
+    sh benchmark_docker.sh -o docker-results-r"$1".txt -p "$OUTDIR"/docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
     echo "Benchmarking Docker with timestep of $1 trial 2"
-    sh benchmark_docker.sh -o docker-results-r"$1".txt -p docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
+    sh benchmark_docker.sh -o docker-results-r"$1".txt -p "$OUTDIR"/docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
     echo "Benchmarking Docker with timestep of $1 trial 3"
-    sh benchmark_docker.sh -o docker-results-r"$1".txt -p docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
+    sh benchmark_docker.sh -o docker-results-r"$1".txt -p "$OUTDIR"/docker-output-r"$1" -r "$1" -w 120 -t 60 -m 2> /dev/null
 }
 run_docker 4
 run_docker 2
@@ -72,11 +75,11 @@ run_docker 0.0625
 # Process
 run_process() {
     echo "Benchmarking process with timestep of $1 trial 1"
-    sh benchmark_process.sh -o process-results-r"$1".txt -p process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_process.sh -o process-results-r"$1".txt -p "$OUTDIR"/process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking process with timestep of $1 trial 2"
-    sh benchmark_process.sh -o process-results-r"$1".txt -p process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_process.sh -o process-results-r"$1".txt -p "$OUTDIR"/process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
     echo "Benchmarking process with timestep of $1 trial 3"
-    sh benchmark_process.sh -o process-results-r"$1".txt -p process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
+    sh benchmark_process.sh -o process-results-r"$1".txt -p "$OUTDIR"/process-output-r"$1" -r "$1" -w 120 -t 60 2> /dev/null
 }
 run_process 4
 run_process 2
